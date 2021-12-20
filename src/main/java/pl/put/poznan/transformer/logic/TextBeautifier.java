247346d688425ff1730733568fec09d7eca1e15e
Maxi351 <nikodem@gamaolejnicki.pl>
2021-12-20 19:22:03 +0100

    Rest for the TextBeautifier

--- src/main/java/pl/put/poznan/transformer/logic/TextBeautifier.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextBeautifier.java
@@ -1,6 +1,5 @@
 package pl.put.poznan.transformer.logic;
 
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
